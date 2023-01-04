7ecb886d2c24ce20e60e98a601093b100477ab02
hyeg <hyeg0121@naver.com>
2023-01-04 10:52:11 +0900

    사과 텍스쳐

--- apple_game/main.cpp
+++ apple_game/main.cpp
@@ -7,11 +7,12 @@
 using namespace sf;
 
 const int W_WIDTH = 1000, W_HEIGHT = 1000;	//윈도우의 크기
-const int A_WIDTH = 30, A_HEIGHT = 30;		//사과의 크기
+const int A_WIDTH = 25, A_HEIGHT = 25;		//사과의 크기
 const int A_AMOUNT = 49;					//사과의 개수 
 
 struct Apples
 {
+	RectangleShape sprite;
 	int num;			//사과 안에 써있는 숫자
 	int is_clicked;		//클릭되어져 있는지
 	int idx;			//사과의 인덱스
@@ -26,6 +27,15 @@ int main(void)
 	/* mouse */
 	Vector2i mouse_pos;
 
+	/* apple */
+	Texture apple_texture[9];
+	char file_name[50];
+	for (int i = 0; i < 9; i++)
+	{
+		sprintf_s(file_name, "./resources/images/apple%d.png", i + 1);
+		apple_texture[i].loadFromFile(file_name);
+
+	}
 	/* 프로그램 실행 중 */
 	while (window.isOpen())
 	{
