1314f6e3bcb9588bc2c9e2d309486add92dcd590
hyeg <hyeg0121@naver.com>
2023-01-04 10:30:46 +0900

    윈도우 기본 구조 생성

--- apple_game/main.cpp
+++ apple_game/main.cpp
@@ -4,7 +4,49 @@
 #include<stdlib.h>
 #include<time.h>
 
+using namespace sf;
+
+const int W_WIDTH = 1000, W_HEIGHT = 1000;
+
 int main(void)
 {
+	/* window*/
+	RenderWindow window(VideoMode(W_WIDTH, W_HEIGHT), "AppleGame");
+	window.setFramerateLimit(60);
+
+	/* mouse */
+	Vector2i mouse_pos;
+
+	/* 프로그램 실행 중 */
+	while (window.isOpen())
+	{
+		/* mouse update */
+		mouse_pos = Mouse::getPosition(window);
+
+		/* event */
+		Event event;
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+				//종료
+			case Event::Closed:
+				window.close();
+				//마우스 좌클릭 
+			case Event::MouseButtonPressed:
+				if (event.mouseButton.button == Mouse::Left)
+				{
+
+				}
+			default:
+				break;
+			}
+		}
+
+		window.clear(Color::White);
+
+		window.display();
+	}
+
 	return 0;
 }
