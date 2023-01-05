7a07ce6fc98b1328468e76056c7017d35d142d32
hyeg <hyeg0121@naver.com>
2023-01-05 09:47:08 +0900

    버튼 생성

--- apple_game/main.cpp
+++ apple_game/main.cpp
@@ -11,6 +11,19 @@ const int A_WIDTH = 90, A_HEIGHT = 90;		//사과의 크기
 const int A_AMOUNT = 81;					//사과의 개수 
 const int TIME_LIMIT = 60;					//제한시간
 
+enum Progress {
+	START,
+	ONGOING,
+	END,
+	SHOW_HOWTOPLAY
+};
+
+enum ButtonType {
+	GAMESTART,
+	HOWTOPLAY,
+	BACK,
+};
+
 struct Apple
 {
 	RectangleShape sprite;
@@ -21,16 +34,16 @@ struct Apple
 	int score;			//점수
 };
 
-enum Progress {
-	START,
-	ONGOING,
-	END
+struct Button {
+	RectangleShape sprite;
+	enum ButtonType type;
+	Texture texture;
 };
 
 int main(void)
 {
 	enum Progress progress;
-	progress = ONGOING;
+	progress = START;
 
 	/* window*/
 	RenderWindow window(VideoMode(W_WIDTH, W_HEIGHT), "AppleGame");
@@ -69,21 +82,19 @@ int main(void)
 	click_sound.setBuffer(click_sb);
 	click_sound.setVolume(70);
 
-	/* gameover */
-	RectangleShape gameover_stripe;
-	Texture gameover_texture;
-	gameover_texture.loadFromFile("./resources/images/gameover.png");
-	gameover_stripe.setPosition(150, 150);
-	gameover_stripe.setSize(Vector2f(700, 700));
-	gameover_stripe.setTexture(&gameover_texture);
-
 	/* background */
 	RectangleShape background_stripe;
-	Texture background_texture;
-	background_texture.loadFromFile("./resources/images/background.png");
+	Texture background_start_texture;
+	background_start_texture.loadFromFile("./resources/images/background_start.png");
+	Texture background_ongoing_texture;
+	background_ongoing_texture.loadFromFile("./resources/images/background_ongoing.png");
+	Texture background_end_texture;
+	background_end_texture.loadFromFile("./resources/images/background_end.png");
+	Texture background_show_texture;
+	background_show_texture.loadFromFile("./resources/images/background_show_howtoplay.png");
 	background_stripe.setPosition(0, 0);
 	background_stripe.setSize(Vector2f(W_WIDTH, W_HEIGHT));
-	background_stripe.setTexture(&background_texture);
+	background_stripe.setTexture(&background_start_texture);
 
 	/* apple */
 	//texture 지정
@@ -109,41 +120,78 @@ int main(void)
 		//sprite
 		apples[i].sprite.setTexture(&apple_texture[apples[i].num - 1]);
 		apples[i].sprite.setSize(Vector2f(A_WIDTH, A_HEIGHT));
-		if (i % 9 == 0) //10번째 사과에서 줄내림
+		if (i % 9 == 0) //9번째 사과에서 줄내림
 			enter += 90;
 		apples[i].sprite.setPosition(90 + (i % 9) * A_WIDTH, enter);
 	}
-	
+
+	/* Button */
+	struct Button btn[3];
+	//시작 버튼
+	btn[0].texture.loadFromFile("./resources/images/game_start.png");
+	btn[0].type = GAMESTART;
+	//게임 설명 버튼
+	btn[1].texture.loadFromFile("./resources/images/show_howtoplay.png");
+	btn[1].type = HOWTOPLAY;
+	//뒤로가기
+	btn[2].texture.loadFromFile("./resources/images/back.png");
+	btn[2].type = BACK;
+	for (int i = 0; i < 3; i++)
+	{
+		btn[i].sprite.setPosition(200 + i * 320, 500);
+		btn[i].sprite.setSize(Vector2f(300, 300));
+		btn[i].sprite.setTexture(&btn[i].texture);
+	}
+	btn[2].sprite.setSize(Vector2f(600, 200));
+
 	start_time = clock();
 
 	/* 프로그램 실행 중 */
 	while (window.isOpen())
 	{
-		switch (progress) {
-		case 0:
-			break;
-		//게임 진행 중
-		case 1:
+		/* mouse update */
+		mouse_pos = Mouse::getPosition(window);
+
+		/* time update */
+		spent_time = clock() - start_time;
+
+		/* event */
+		Event event;
+		while (window.pollEvent(event))
 		{
-			/* mouse update */
-			mouse_pos = Mouse::getPosition(window);
-
-			/* time update */
-			spent_time = clock() - start_time;
-
-			/* event */
-			Event event;
-			while (window.pollEvent(event))
+			switch (event.type)
 			{
-				switch (event.type)
+				//종료
+			case Event::Closed:
+				window.close();
+				//마우스 좌클릭 
+			case Event::MouseButtonPressed:
+				switch (progress)
 				{
-					//종료
-				case Event::Closed:
-					window.close();
-					//마우스 좌클릭 
-				case Event::MouseButtonPressed:
+				case 0:
+				{
+					for (int i = 0; i < 2; i++)
+					{
+						//버튼을 클릭했다면
+						if (btn[i].sprite.getGlobalBounds().contains(mouse_pos.x, mouse_pos.y))
+						{
+							switch (btn[i].type)
+							{
+							case GAMESTART:
+								progress = ONGOING;
+								break;
+							case HOWTOPLAY:
+								progress = SHOW_HOWTOPLAY;
+								break;
+							}
+						}
+					}
+				}//case 0
+				case 1:
+				{
 					if (event.mouseButton.button == Mouse::Left)
 					{
+						click_sound.play();
 						for (int i = 0; i < A_AMOUNT; i++)
 						{
 							//사과를 클릭했다면
@@ -153,15 +201,39 @@ int main(void)
 								apples[i].is_clicked = 1;
 								judge += apples[i].num;
 								++click_cnt;
-								click_sound.play();
 							}
 						}
+
+					}//if
+				}
+				case 3:
+				{
+					if (btn[2].sprite.getGlobalBounds().contains(mouse_pos.x, mouse_pos.y))
+					{
+						progress = START;
 					}
-				default:
 					break;
 				}
-			}
-
+				}//case 3
+				default:
+					break;
+				}//switch
+			}//switch
+		//while
+		switch (progress) {
+		//게임 시작
+		// TODO : 게임 시작 부분 구현하기
+		case 0:
+			window.clear();
+			background_stripe.setTexture(&background_start_texture);
+			window.draw(background_stripe);
+			window.draw(btn[0].sprite);
+			window.draw(btn[1].sprite);
+			break;
+		//게임 진행 중
+		case 1:
+		{
+			background_stripe.setTexture(&background_ongoing_texture);
 			/* apple update */
 			//여러 개의 사과가 클릭 된 상태 
 			if (judge == 10)
@@ -210,20 +282,44 @@ int main(void)
 
 			if (is_gameover)
 			{
-				window.draw(gameover_stripe);
-				sprintf_s(info, "SCORE : %d", score);
-				text.setString(info);
-				text.setPosition(400, 600);
-
+				progress = END;
 			}
 			window.draw(text);
-			window.display();
+			
 		}
 			break;
+		//게임 종료
 		case 2:
+		{
+			background_stripe.setTexture(&background_end_texture);
+
+			window.draw(background_stripe);
+
+			sprintf_s(info, "SCORE : %d", score);
+			text.setString(info);
+			text.setPosition(400, 600);
+			window.draw(text);
+
+			if (Keyboard::isKeyPressed(Keyboard::Space))
+			{
+				progress = START;
+			}
 			break;
 		}
-		
+		//SHOW_HOWTOPLAY
+		case 3: 
+		{
+			window.clear();
+			background_stripe.setTexture(&background_show_texture);
+			window.draw(background_stripe);
+
+			btn[2].sprite.setPosition(200, 800);
+			window.draw(btn[2].sprite);
+
+		}
+		break;
+		}//switch
+		window.display();
 	}
 
 	return 0;
